85c7b0b94a6b42db92a5d78f890783076d8e854f
samiulislam07 <samiul.islam07@northsouth.edu>
2023-01-30 21:31:15 +0600

    Skipped staging and fixed<

--- BankingSystem.c
+++ BankingSystem.c
@@ -5,14 +5,29 @@
 typedef struct customer
 {
     char name[100];
-    int accNo[10];
-    int mobileNo[11];
+    char accNo[10];
+    char mobileNo[11];
     double balance;
 } cust;
 
+int numCustomers;
+
+void depositMoney(char acc[10], cust *p, double amount)
+{
+    for(int i=0; i<numCustomers; i++)
+    {
+        p = p+i;
+        if(!strcmp(acc, p->accNo))
+        {
+            p->balance= amount;
+        }
+    }
+    
+}
+
 void main()
 {
-    int numCustomers;
+    
     scanf("%d", &numCustomers);
 
     if(numCustomers<=0 || numCustomers>100)
@@ -36,6 +51,41 @@ void main()
         scanf("%s", ptr->mobileNo);
         printf("Enter balance: ");
         scanf("%lf", &ptr->balance);
+
+    }
+
+    ptr = ptr-(numCustomers-1);
+
+    int option;
+    while(1)
+    {
+        printf("Please select an option-\nChoose 1 to deposit money.\nChoose 2 to withdraw money.\nChoose 3 to sort all customers using their salary.
+        \nChoose 4 to search customers by using their mobileNo.\nChoose 5 to print all the customers.\nChoose 6 to print the sum Of all customer balances.
+        \nChoose 0 to exit.\nEnter you option: ");
+
+        scanf("%d", &option);
+        if(option==0)
+        {
+            break;
+        }
+
+        switch (option)
+        {
+        case 1: char acc[10];
+        printf("Enter account number: ");
+        scanf("%s", acc);
+        double amount;
+        printf("Enter the balance: ");
+        scanf("%lf", &amount);
+        depositMoney(acc, ptr, amount);
+
+            break;
+
+        case 2: 
+        
+        default:
+            break;
+        }
 
     }
 }
